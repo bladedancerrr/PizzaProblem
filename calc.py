d8286e1e7db4ddd27576fb70db909cf3d50ab65a
Ayesha Ahmed <ay3sha.256@gmail.com>
2019-02-21 17:34:49 +1100

    it be ayesha

---
+++ calc.py
@@ -0,0 +1,11 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+def divide(x, y):
+    if y == 0: 
+        raise ValueError('Can not divide by zero!')
+    return x/y
