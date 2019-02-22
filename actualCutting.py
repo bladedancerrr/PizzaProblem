60678a97ad67b1765201d43b5c78f95c0157cbb4
yaoshi <yaos5@student.unimelb.edu.au>
2019-02-22 16:20:29 +1100

    Update actualCutting.py

---
+++ actualCutting.py
@@ -0,0 +1,11 @@
+# written by Peter Shi
+import pizzaStructure
+
+
+def perform_actual_cutting(slice, dimension_tuple):
+    horizontal_cutpoints = dimension_tuple[0]
+    vertical_cutpoints = dimension_tuple[1]
+
+    
+
+    pass
