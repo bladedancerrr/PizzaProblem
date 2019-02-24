4a0173ce32d52366686e12b49ef5dcc0a6076575
Lexon <lexonli@hotmail.com>
2019-02-24 19:36:28 +1100

    FINALLY

--- main.py
+++ main.py
@@ -9,21 +9,18 @@ from actualCutting import perform_actual_cutting
 def main(fileName):
 	pizza = Pizza(fileName)
 	shape = pizzaShape(pizza)
-	print(shape)
 	dimension_list = determineCutPoints((pizza.rows, pizza.cols), shape)
 	slices = perform_actual_cutting(dimension_list)
-	print("amount of slices: ", len(slices))
-	for i in range(len(slices)):
-		slices[i].display(pizza)
-		print(is_validate_slice(pizza, slices[i]))
-		if is_validate_slice(pizza, slices[i]):
-			print("Valid")
-		else:
-			slices.remove(slices[i])
-			print("Invalid")
-			
-	print("Times run: ", i)
-	print_file(slices)
+	validSlices = []
+	for slice in slices:
+		print("Cut out this: ", pizza.cut(slice), "\n")
+		print("From this pizza: ") 
+		slice.display(pizza)
+		print("\n")
+		print("valid Slice? ", is_validate_slice(pizza, slice))
+		if is_validate_slice(pizza, slice):
+			validSlices.append(slice)
+	print_file(validSlices)
 
 
 if __name__ == '__main__':
