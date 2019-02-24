9ef3cc9a8fcfefeac9b19cef2f6a8680e81907c9
yaoshi <yaos5@student.unimelb.edu.au>
2019-02-24 16:49:43 +1100

    Update writingFile.py

--- writingFile.py
+++ writingFile.py
@@ -1,11 +1,11 @@
 from sliceStructure import Slice
 
-def print_file(file, slices):
+def print_file(slices):
 	"""
 	This function creates an output file
 	of the correct format from the slices
 	"""
-	f = open(file, 'w') #opening file as f in writing mode
+	f = open("Output.txt", 'w') #opening file as f in writing mode
 	f.write(f"{len(slices)}\n") #number of slices in slices list
 	for slice in slices:
 		f.write(f"{slice.startRow} {slice.startCol} {slice.endRow} {slice.endCol}\n")
