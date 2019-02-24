d02b80f750a50b4d4f38e5528b66a55b8ac80c21
yaoshi <yaos5@student.unimelb.edu.au>
2019-02-24 16:54:49 +1100

    Update writingFile.py

--- writingFile.py
+++ writingFile.py
@@ -11,3 +11,9 @@ def print_file(slices):
 		f.write(f"{slice.startRow} {slice.startCol} {slice.endRow} {slice.endCol}\n")
 
 		
+if __name__ == "__main__":
+    a = Slice(0, 0, 2, 2)
+    b = Slice(3, 3, 5, 5)
+    c = Slice(6, 6, 8, 8)
+
+    print_file([a, b, c])
