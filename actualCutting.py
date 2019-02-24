732211753068859366c763a86d28b8b70a95f70b
yaoshi <yaos5@student.unimelb.edu.au>
2019-02-24 16:12:05 +1100

    it deals with generating slice objects

--- actualCutting.py
+++ actualCutting.py
@@ -1,11 +1,20 @@
 # written by Peter Shi
-import pizzaStructure
+from sliceStructure import Slice
 
 
-def perform_actual_cutting(slice, dimension_tuple):
-    horizontal_cutpoints = dimension_tuple[0]
-    vertical_cutpoints = dimension_tuple[1]
+def perform_actual_cutting(dimension_tuple):
+    horizontal_dimension = dimension_tuple[0]
+    vertical_dimension = dimension_tuple[1]
 
+    slice_list = list()
 
-    print()
-    pass
+    assert len(horizontal_dimension) == len(vertical_dimension)
+    for i in range(len(horizontal_dimension)):
+        s_r = horizontal_dimension[i][0]
+        e_r = horizontal_dimension[i][0]
+        s_c = vertical_dimension[i][0]
+        e_c = vertical_dimension[i][0]
+
+        new_slice = Slice(s_r, s_c, e_r, e_c)
+        slice_list.append(new_slice)
+    return slice_list
--- visualisation.py
+++ visualisation.py
@@ -1,1 +1,4 @@
-def visualise():
+def visualise(pizza):
+    """
+
+    """
